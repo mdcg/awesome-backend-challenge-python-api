8c75d540fa17191005b49ec6733a7ec5ec306662
Mauro de Carvalho <mauro.dcg@gmail.com>
2019-05-28 18:36:35 -0300

    Creating project's constants module

---
+++ api/constants.py
@@ -0,0 +1,11 @@
+READY = 'ready'
+PRODUCTION = 'production'
+CLOSING = 'closing'
+SENT = 'sent'
+
+STATUS = (
+    (READY, READY),
+    (PRODUCTION, PRODUCTION),
+    (CLOSING, CLOSING),
+    (SENT, SENT),
+)
